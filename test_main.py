a4bd709b8aa029206d224f3616b0a940afa839b9
spaces1233 <spaces1233@gmail.com>
2024-11-21 02:40:47 -0500

    Update test_main.py
    Added both pass and fail tests

--- test_main.py
+++ test_main.py
@@ -2,12 +2,22 @@ import pytest
 from main import add_numbers
 
 def test_add_numbers():
-    assert add_numbers(2, 3) == 5
-    assert add_numbers(-1, 1) == 0
-    assert add_numbers(0, 0) == 0
+    # This test will pass
+    assert add_numbers(2, 3) == 5  # Pass: 2 + 3 = 5
+    
+    # This test will fail: expected result is incorrect
+    assert add_numbers(-1, 1) == 1  # Fail: It should be 0, but we expect 1
 
 def test_add_large_numbers():
-    assert add_numbers(10**6, 10**6) == 2000000  # Test with large numbers
+    # This test will pass
+    assert add_numbers(10**6, 10**6) == 2000000  # Pass: 10^6 + 10^6 = 2000000
+    
+    # This test will fail: expected result is incorrect
+    assert add_numbers(10**6, 10**6) == 1500000  # Fail: It should be 2000000, but we expect 1500000
 
 def test_add_floating_point_numbers():
-    assert add_numbers(2.5, 3.5) == 6.0  # Test with floating point numbers
+    # This test will pass
+    assert add_numbers(2.5, 3.5) == 6.0  # Pass: 2.5 + 3.5 = 6.0
+    
+    # This test will fail: expected result is incorrect
+    assert add_numbers(0.1, 0.2) == 0.4  # Fail: It should be 0.3, but we expect 0.4
