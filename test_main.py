f2dc222a2198be38e96b409a0faf73fce212378c
spaces1233 <spaces1233@gmail.com>
2024-11-22 13:00:41 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,12 +4,12 @@ from main import add_numbers
 def test_add_numbers():
     assert add_numbers(2, 3) == 5
     
-    assert add_numbers(-1, 1) == 1
+    assert add_numbers(-1, 1) == 0
 
 def test_add_large_numbers():
-    assert add_numbers(10**6, 2 * 10**6) == 2000000
+    assert add_numbers(10**6, 2 * 10**6) == 3000000
     
-    assert add_numbers(10**6, 10**6) == 1500000
+    assert add_numbers(10**6, 10**6) == 2000000
 
 def test_add_floating_point_numbers():
     assert add_numbers(2.5, 3.5) == 6.0
