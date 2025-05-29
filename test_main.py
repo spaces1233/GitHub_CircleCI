aeb8af002623a6aa2ed7fbcf8d0a7a4463cc21bf
spaces1233 <spaces1233@gmail.com>
2025-05-29 06:45:40 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@ import pytest
 from main import add_numbers
 
 def test_add_numbers():
-    assert add_numbers(3, 3) == 5
+    assert add_numbers(2, 3) == 5
     
     assert add_numbers(-1, 1) == 0
 
